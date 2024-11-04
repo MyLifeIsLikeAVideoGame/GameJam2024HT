3b0c1f5834faed70f4a58410fd454fb0fc88bdcb
V0id51 <chennakeshav.rachannagari@elev.ga.lbs.se>
2024-11-04 12:40:10 +0100

    movement och camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput.normalized * moveSpeed;
+    }
+}
