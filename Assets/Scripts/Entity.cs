f404df903828679cafaaa242b58a9847ec4a8866
V0id51 <chennakeshav.rachannagari@elev.ga.lbs.se>
2024-11-04 14:07:22 +0100

    debug room plus changes

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int health;
+    public int xpDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public virtual void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float size;
+    public LayerMask whatIsSolid;
+    public int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit2D touchingSolid = Physics2D.Raycast(transform.position, Vector2.up * size, whatIsSolid);     
+        if (touchingSolid)
+        {
+            if (touchingSolid.collider.GetComponent<Entity>() != null)
+            {
+                touchingSolid.collider.GetComponent<Entity>().TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        
+            Gizmos.DrawWireCube(transform.position, Vector3.up * size);
+        
+    }
+}
+
+
