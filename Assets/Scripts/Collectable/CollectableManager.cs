7059a5fef31eac556830d6dc23cbc43931aa3b4d
MyLifeIsLikeAVideoGame <alexyanxuan.tian@elev.ga.lbs.se>
2024-11-05 11:43:44 +0100

    Point Collect

--- Assets/Scripts/Collectable/CollectableManager.cs
+++ Assets/Scripts/Collectable/CollectableManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectableManager : MonoBehaviour
 {
     public int keysCollected = 0;
     public int pointsCollected = 0;
+    public TextMeshProUGUI pointDisplay;
+
+    private void Update()
+    {
+        pointDisplay.text = "Coins: " + pointsCollected;
+    }
 }
