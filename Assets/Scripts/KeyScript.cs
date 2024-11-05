7b8162bcf05a78d40970194481e9bd39fb2c9a35
MyLifeIsLikeAVideoGame <alexyanxuan.tian@elev.ga.lbs.se>
2024-11-05 09:34:10 +0100

    Collectable base done

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    public int keysCollected = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+
+}
