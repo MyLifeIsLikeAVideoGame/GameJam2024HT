e558a1c0a8f73e7db82af70d3ec774e3cc01d163
V0id51 <chennakeshav.rachannagari@elev.ga.lbs.se>
2024-11-06 11:36:55 +0100

    working boss

--- Assets/Scripts/DestroyAfterWait.cs
+++ Assets/Scripts/DestroyAfterWait.cs
@@ -2,26 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShieldBossSpear : MonoBehaviour
+public class DestroyAfterWait : MonoBehaviour
 {
     public float waitTime;
-    Rigidbody2D rb;
-    public Vector2 vel;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        Invoke("SetVelocity", waitTime);
-
+        Destroy(gameObject, waitTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public void SetVelocity()
-    {
-        rb.velocity = vel;
     }
 }
