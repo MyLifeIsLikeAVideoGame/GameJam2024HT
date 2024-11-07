61db3fee1cdae8b21c3504a6cd435e246f1fe4f7
V0id51 <chennakeshav.rachannagari@elev.ga.lbs.se>
2024-11-07 09:46:53 +0100

    added some more save options

--- Assets/Scripts/Collectable/CollectableManager.cs
+++ Assets/Scripts/Collectable/CollectableManager.cs
@@ -9,9 +9,17 @@ public class CollectableManager : MonoBehaviour
     public int keysCollected = 0;
     public int pointsCollected = 0;
     public TextMeshProUGUI pointDisplay;
+    public IntValue keysSave, pointsSave;
+    private void Start()
+    {
+        keysCollected = keysSave.value;
+        pointsCollected = pointsSave.value;
+    }
 
     private void Update()
     {
         pointDisplay.text = ": " + pointsCollected;
+        keysSave.value = keysCollected;
+        pointsSave.value = pointsCollected;
     }
 }
